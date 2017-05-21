6a65edc812ba23e3dc78744ab0d37ddc9045e7d2
Arun Singh <arunsingh.in@gmail.com>
2017-05-22 00:48:39 +0530

    tf main edited

--- terraform-aws/site.tf
+++ terraform-aws/site.tf
@@ -1,4 +1,4 @@
-# Provider spcific
+# Provider public cloud specific
 provider "aws" {
     region = "${var.aws_region}"
 }
@@ -6,19 +6,19 @@ provider "aws" {
 # Variables for VPC module
 module "vpc_subnets" {
 	source = "./modules/vpc_subnets"
-	name = "tendo"
+	name = "cleo"
 	environment = "dev"
 	enable_dns_support = true
 	enable_dns_hostnames = true
 	vpc_cidr = "172.16.0.0/16"
         public_subnets_cidr = "172.16.10.0/24,172.16.20.0/24"
         private_subnets_cidr = "172.16.30.0/24,172.16.40.0/24"
-        azs    = "eu-west-1a,eu-west-1b"
+        azs    = "ap-southeast-1a,ap-southeast-1b"
 }
 
 module "ssh_sg" {
 	source = "./modules/ssh_sg"
-	name = "tendo"
+	name = "cleo"
 	environment = "dev"
 	vpc_id = "${module.vpc_subnets.vpc_id}"
 	source_cidr_block = "0.0.0.0/0"
@@ -26,7 +26,7 @@ module "ssh_sg" {
 
 module "web_sg" {
 	source = "./modules/web_sg"
-	name = "tendo"
+	name = "cleo"
 	environment = "dev"
 	vpc_id = "${module.vpc_subnets.vpc_id}"
 	source_cidr_block = "0.0.0.0/0"
@@ -34,14 +34,14 @@ module "web_sg" {
 
 module "elb_sg" {
 	source = "./modules/elb_sg"
-	name = "tendo"
+	name = "cleo"
 	environment = "dev"
 	vpc_id = "${module.vpc_subnets.vpc_id}"
 }
 
 module "rds_sg" {
     source = "./modules/rds_sg"
-    name = "tendo"
+    name = "cleo"
     environment = "dev"
     vpc_id = "${module.vpc_subnets.vpc_id}"
     security_group_id = "${module.web_sg.web_sg_id}"
@@ -49,16 +49,16 @@ module "rds_sg" {
 
 module "ec2key" {
 	source = "./modules/ec2key"
-	key_name = "tendo"
-	public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAixOG1qLaK0YisTWKui8vgcaBITkioVsHKYgwvcqp4z91LW6TO9fnSVZHUIW232g0rAQCVv9owGH4kfWRukH4hs4UkA4A+9m4ra2cS8fJDJ066zmLiwUT+F/5XynWVQ+bN8koewDyqAHjplYWqYiVGPvRqoYPbXqgwwS4tSK/7Ce23+9nBEenTzhtPIDCnCyNI6d9QhSiZZqR5uRJJbWA+MMqJJpSrW2CQeHjDKo766kelVdihpCf2MCI+XWqBDN7DmsysocnsvMZ5m8dxWSkg1GigEV07iw58Ge+8q9StQRRz5XV/nqo9xjAPCOdIkVAQH+e2xFHlJoIJU/2WNH3Jw=="
+	key_name = "cleo"
+	public_key = "ssh-dss AAAAB3NzaC1kc3MAAACBAPSMmmBrUEiGB33P3mQNUUFJkxKO3jdmKRh3+ivJMCd4V0dUWOdT5Qixg5/+/vvz2ispxq7AsWOb3IUP/Bj0zugBBFP0HCNoV3LoSBymb78HgOpJN8qxCUYPUrI6P0i+J/LtMsqxsFWrebFr5RnzFiPDGvSnD8KRqQc9ivXHPx/BAAAAFQDK61xWL5rJyAvtFn7RuOJ0Sq5kDwAAAIEAgnPBVipA/smVOoQ/GJFRYRywFbZmiq8ECTaw8hBs0RNgPJTn4Jiq1fMeXpadMbOg3TY36LpaniqQQn/6kxh7xw8swJohAVN7u28zkXHEdMrsvwx07/weuMYQOhlj5oJ0tLN8+rDqeV9NiCrfcToQMv1XtIvuf5R/cVDjSfeNRfcAAACBAM2dmyO3grfBPlW/mKcQk3Ag97JE1hvzqtAsnhFhXWkS3LAqDuiT62QQi8+XxhxlP9gxl0fvf6Ko/9R2IIGVtih91p7kDi5ZDFzranDrYsaXxtSjm9Bz1Xv4fELyA9iJ19wkXUfc+GGn9H++RuwWFtxY4obvAYXqmusHUmp4mp+j arunsingh.in@gmail.com"
 }
 
 module "ec2" {
 	source = "./modules/ec2"
-	name = "tendo"
+	name = "cleo"
 	environment = "dev"
 	server_role = "web"
-	ami_id = "ami-f95ef58a"
+	ami_id = "ami-9b8c0bf8"
 	key_name = "${module.ec2key.ec2key_name}"
 	count = "2"
 	security_group_id = "${module.ssh_sg.ssh_sg_id},${module.web_sg.web_sg_id}"
@@ -69,7 +69,7 @@ module "ec2" {
 
 module "rds" {
 	source = "./modules/rds"
-	name = "tendo"
+	name = "cleo"
 	environment = "dev"
 	storage = "5"
 	engine_version = "5.6.27"
@@ -82,10 +82,10 @@ module "rds" {
 
 module "elb" {
 	source = "./modules/elb"
-	name = "tendo"
+	name = "cleo"
 	environment = "dev"
 	security_groups = "${module.elb_sg.elb_sg_id}"
-	availability_zones = "eu-west-1a,eu-west-1b"
+	availability_zones = "ap-southeast-1a,ap-southeast-1b"
 	subnets = "${module.vpc_subnets.public_subnets_id}"
 	instance_id = "${module.ec2.ec2_id}"
 }
